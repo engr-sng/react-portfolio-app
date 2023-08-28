63186f31a04384565defc896889002937f490cd7
ShingoIshikawa <108569722+engr-sng@users.noreply.github.com>
2023-08-28 17:43:14 +0900

    スキルのレーダーチャートを作成

--- src/pages/homes/top.tsx
+++ src/pages/homes/top.tsx
@@ -6,8 +6,61 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 import MV from "../../assets/images/mv.jpg"
+import Skill from "../../components/Skill"
 
 const Top: React.FC = () => {
+    const setSkillLists = [
+        {
+            labels: ["HTML / CSS ", "SASS / SCSS", "JavaScript", "TypeScript", "React", "Next.js"],
+            datasets: [{
+                label: "Front-End",
+                data: [3, 3, 2, 2, 2, 2],
+                fill: true,
+                backgroundColor: "rgba(75, 192, 192, 0.2)",
+                borderColor: "rgb(75, 192, 192, 1.0)",
+            }],
+        },
+        {
+            labels: ["PHP", "Laravel", "Ruby", "Ruby on Rails", "python", "Django"],
+            datasets: [{
+                label: "Back-End",
+                data: [3, 3, 3, 3, 2, 2],
+                fill: true,
+                backgroundColor: "rgba(54, 162, 235, 0.2)",
+                borderColor: "rgba(54, 162, 235, 1.0)",
+            }]
+        },
+        {
+            labels: ["AWS", "GCP", "Linux", "Windows", "Nginx", "Apache"],
+            datasets: [{
+                label: "DevOps",
+                data: [2, 1, 2, 1, 2, 2],
+                fill: true,
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor: "rgba(255, 99, 132, 1.0)",
+            }]
+        }
+    ];
+
+    const setSkillChartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: true
+            }
+        },
+        scales: {
+            r: {
+                max: 3,
+                min: 0,
+                ticks: {
+                    stepSize: 1
+                },
+            },
+        }
+    }
+
     return(
         <>
             <Box sx={{ height: "65vh", backgroundImage: "url(" + MV + ")", backgroundSize: "cover", backgroundPosition: "center", position: "relative" }}>
@@ -37,15 +90,15 @@ const Top: React.FC = () => {
                                 Skill
                             </Typography>
                         </Grid>
-                        <Grid item xs={12} md={4}>
-                            <Box sx={{ height: 200, backgroundColor: 'red' }}></Box>
-                        </Grid>
-                        <Grid item xs={12} md={4}>
-                            <Box sx={{ height: 200, backgroundColor: 'red' }}></Box>
-                        </Grid>
-                        <Grid item xs={12} md={4}>
-                            <Box sx={{ height: 200, backgroundColor: 'red' }}></Box>
-                        </Grid>
+                        {setSkillLists.map((data) => {
+                            return (
+                                <Grid item xs={12} md={4}>
+                                    <Box sx={{ width: "100%", height: {xs: "400px", md: "200px"} }}>
+                                        <Skill options={setSkillChartOptions} data={data}/>
+                                    </Box>
+                                </Grid>
+                            );
+                       })}
                         <Grid item xs={12} md={12} sx={{ textAlign: 'center' }}>
                             <Button variant="contained" size="large">
                                 LearnMore
